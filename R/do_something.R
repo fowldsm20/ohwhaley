2001c3624e044f2602eb9013b5a28cc51c5651d8
Myles Fowlds <myles.fowlds@students.mq.edu.au>
2021-09-06 11:12:34 +1000

    Initial commit

---
+++ R/do_something.R
@@ -0,0 +1,8 @@
+#' Imagine this does something useful
+#'
+#' @param some_Var
+#' @return a dataframe with results
+do_something <- function(some_var = "abc") {
+  return(data.frame(a = some_var))
+}
+
